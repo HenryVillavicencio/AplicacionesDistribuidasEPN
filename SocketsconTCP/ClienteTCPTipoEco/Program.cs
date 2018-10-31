6472f15736ea7fc72dac70a105fd6110d9a0566d
Henry Villavicencio <henryvillavicencio@live.com>
2018-10-31 17:20:19 -0500

    nn

---
+++ SocketsconTCP/ClienteTCPTipoEco/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClienteTCPTipoEco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            //Se crea un string que contendrá los datos a enviar, y también
+            //se crea un punto remoto que representará al servidor que nos vamos a conectar
+            //de igual forma se crea el cliente tcp, el buffer de recepcion para recibir el mensaje
+            //de eco que enviará el servidor
+            string datos = "##--##--##----***----##--##--##";
+            IPEndPoint remoto = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000);
+            TcpClient cliente = new TcpClient();
+            byte[] bufferRx = new byte[512];
+            int indicador;
+            // Se envía la solicitud de conexión al servidor
+            cliente.Connect(remoto);
+            //Una vez que el servidor acepta la conexión, se obtiene el flujo mediante el cual se 
+            //enviarán los datos contenidos en el buffer de transmisión
+            if (cliente.Connected)
+            {
+                NetworkStream flujo = cliente.GetStream();
+                byte[] bufferTx = Encoding.ASCII.GetBytes(datos);
+                flujo.Write(bufferTx, 0, bufferTx.Length);
+                //Se obtiene la cantidad de datos que se encuentran en el buffer de recepción
+                //si existen datos en el buffer, se codifican de forma adecuada de tal forma que 
+                //puedan ser mostrados en pantalla, esto se repite hasta que ya no existan datos 
+                //en el buffer de recepción.
+                do
+                {
+                    indicador = flujo.Read(bufferRx, 0, bufferRx.Length);
+                    if (indicador > 0)
+                    {
+                        datos = Encoding.ASCII.GetString(bufferRx);
+                        Console.WriteLine("Mensaje Recibido");
+                        Console.WriteLine("Se recibio: \n{0}", datos);
+                    }
+                    Console.WriteLine("FIN{0}", indicador);
+
+                } while (indicador > 0);
+                Console.WriteLine("FIN{0}", indicador);
+
+                //al finalizar se cierra la conexión
+                flujo.Close();
+                cliente.Close();
+
+
+            }
+        }
+    }
+}
---
+++ SocketsconTCP/ServidorTCP/Program.cs
@@ -0,0 +1,74 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServidorTCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creamos un conjunto de objetos y variables que nos permitiran 
+            // almacenar y majenar las conexiones del cliente, servidor, asi como los buffer
+            // y datos a enviar.
+
+            TcpClient manejoCliente;
+            TcpListener servidor;
+
+            //### Cambie el tamaño del buffer de recepción a 1. Realice pruebas
+            //### Para ver el funcionamiento del programa original  descomente las
+            //### lineas //#  y comente las restantes hasta el delimitador //### 
+        
+            //# byte[] bufferRx = new byte[512];
+   
+            byte[] bufferRx = new byte[1];
+            
+            // ###
+
+            int datosLeidos;
+            string datos;
+
+
+            //creo el socket local mediante el cuál escuchará el servidor en el 
+            //puerto 11000 y cualquier ip. Iniciamos con el método start la escucha
+            
+            IPEndPoint puntoLocal = new IPEndPoint(IPAddress.Any, 11000);
+            servidor = new TcpListener(puntoLocal);
+            Console.WriteLine("El servidor está escuchado...");
+            servidor.Start();
+            //mediante esta condición hacemos que el servidor permanezca escuchando 
+            //las solicitudes de conexión permanentemente. En este caso no se puede
+            //tener conexiones simultáneas.
+            
+            while (true)
+            {
+                //Aceptamos la conexión por parte del cliente, y obtenemos el flujo
+                //del cual recuperaremos la información.
+                manejoCliente = servidor.AcceptTcpClient();
+                Console.WriteLine("El servidor ha aceptado a un cliente...");
+                NetworkStream flujo = manejoCliente.GetStream();
+
+                //Si es que existen datos,leemos el flujo de datos y los mostramos en consolo, y repetimos el proceso
+                //hasta que no hayamos terminado de leer todos los datos.
+                do
+                {
+                    datosLeidos = flujo.Read(bufferRx, 0, bufferRx.Length);
+                    if (datosLeidos > 0)
+                    {
+                        datos = Encoding.ASCII.GetString(bufferRx);
+                        Console.WriteLine("Mensaje Recibido");
+                        Console.WriteLine("Se recibio: \n{0}", datos);
+                    }
+                } while (datosLeidos > 0);
+            }
+        }
+
+    }
+}
